d7efee784f38e5f8feae761b8e1fb46dfce84461
kanaru-ssk <2019january.0131@gmail.com>
2023-03-26 20:39:18 +0900

    add font minion as serif

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const fontMinion = localFont({
+  src: "./MinionVariableConcept.otf",
+  variable: "--font-minion",
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={fontMinion.variable}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default App;
