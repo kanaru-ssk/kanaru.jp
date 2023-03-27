eb7c045ffb37af4571c75c4c08ce401b8296a680
kanaru-ssk <2019january.0131@gmail.com>
2023-03-28 01:46:32 +0900

    fix fill param to color code

--- src/components/atoms/CloseIcon.tsx
+++ src/components/atoms/CloseIcon.tsx
@@ -1,6 +1,6 @@
 const CloseIcon = () => {
   return (
-    <svg width="48" height="48" fill="black">
+    <svg width="48" height="48" fill="#000">
       <path d="M24 22.5454L17.4546 16L16 17.4545L22.5455 24L16 30.5455L17.4545 32L24 25.4545L30.5455 32L32 30.5454L25.4545 24L32 17.4546L30.5454 16L24 22.5454Z" />
     </svg>
   );
--- src/components/atoms/MenuIcon.tsx
+++ src/components/atoms/MenuIcon.tsx
@@ -1,6 +1,6 @@
 const MenuIcon = () => {
   return (
-    <svg width="48" height="48" fill="black">
+    <svg width="48" height="48" fill="#000">
       <rect x="14" y="23" width="20" height="2" />
       <rect x="14" y="17" width="20" height="2" />
       <rect x="14" y="29" width="20" height="2" />
