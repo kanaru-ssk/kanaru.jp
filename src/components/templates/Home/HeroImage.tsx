836d6f1deb365723012de43405e036687369a102
kanaru-ssk <2019january.0131@gmail.com>
2023-03-27 21:29:19 +0900

    fix style of HeroImage component

--- src/components/templates/Home/HeroImage.tsx
+++ src/components/templates/Home/HeroImage.tsx
@@ -2,8 +2,8 @@ import ScrollIcon from "@/components/atoms/ScrollIcon";
 
 const HeroImage = () => {
   return (
-    <section className="flex h-screen w-screen flex-col items-center justify-between py-32">
-      <div className="">
+    <section className="flex h-screen flex-col items-center justify-between space-y-4 py-32">
+      <div>
         <p className="text-gray-500">I&apos;m</p>
         <h1 className="font-serif text-4xl font-bold">Kanaru Sasaki</h1>
         <p className="text-gray-500">Front-End Web Developer</p>
