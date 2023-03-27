6940b3c0a9b2469910a7649ac2df0d1c33060533
kanaru-ssk <2019january.0131@gmail.com>
2023-03-27 21:32:54 +0900

    add Link to Header Logo

--- src/components/templates/Home/Header/index.tsx
+++ src/components/templates/Home/Header/index.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 import Nav from "./Nav";
 
 const Header = () => {
   return (
     <header className="fixed top-0 z-10 flex h-12 w-full items-center justify-between bg-white px-2 sm:h-20 sm:px-8">
-      <Image
-        src="/icon.svg"
-        alt="icon"
-        width={32}
-        height={32}
-        className="h-12 w-12 p-2 sm:h-20 sm:w-20"
-      />
+      <Link href="/" title="top">
+        <Image
+          src="/icon.svg"
+          alt="icon"
+          width={32}
+          height={32}
+          className="h-12 w-12 p-2 sm:h-20 sm:w-20"
+        />
+      </Link>
       <Nav />
     </header>
   );
