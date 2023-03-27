5465011b874222f78226802a115135103e0b3a0f
kanaru-ssk <2019january.0131@gmail.com>
2023-03-27 21:22:31 +0900

    fix scroll behavior

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
-    <Html lang="en">
+    <Html lang="en" className="scroll-pt-12 scroll-smooth sm:scroll-pt-20">
       <Head />
       <body>
         <Main />
