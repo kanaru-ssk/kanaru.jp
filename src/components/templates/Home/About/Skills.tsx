ad92c27982f919c929c3960aa502d0a14c7cf47d
kanaru-ssk <2019january.0131@gmail.com>
2023-03-30 13:21:08 +0900

    fix styles

--- src/components/templates/Home/About/Skills.tsx
+++ src/components/templates/Home/About/Skills.tsx
@@ -6,7 +6,7 @@ const Skills = ({ skills }: Props) => {
   return (
     <span className="flex flex-wrap gap-2">
       {skills.map((skill) => (
-        <span key={skill} className="bg-gray-100 py-2 px-3 font-mono text-xs">
+        <span key={skill} className="bg-gray-100 py-2 px-3 font-sans text-xs">
           {skill}
         </span>
       ))}
