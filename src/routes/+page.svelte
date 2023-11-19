9742ebcc64cd29d5856797ae5b18f79562f0ce70
kanaru-ssk <2019january.0131@gmail.com>
2023-11-19 12:42:29 +0900

    delete wasm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-	import FirstView from '$lib/components/FirstView.svelte';
-	import Wasm from '$lib/components/Wasm.svelte';
+  import FirstView from "$lib/components/FirstView.svelte";
 </script>
 
-<svelte:head>
-	<title>佐々木哉瑠</title>
-</svelte:head>
-
 <FirstView />
-<Wasm />
