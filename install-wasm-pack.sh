40452e05e3bd6436a2f79214d7deced7aff5ca50
kanaru-ssk <2019january.0131@gmail.com>
2023-11-21 01:22:41 +0900

    fix install script

--- install-wasm-pack.sh
+++ install-wasm-pack.sh
@@ -1,3 +1,3 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
+source "/vercel/.cargo/env"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
