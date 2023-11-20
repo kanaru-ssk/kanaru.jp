693008bcb725f7e429926c87a8ecc1c4db9eb488
kanaru-ssk <2019january.0131@gmail.com>
2023-11-21 02:01:10 +0900

    add script for debug

--- install-wasm-pack.sh
+++ install-wasm-pack.sh
@@ -3,7 +3,14 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 echo "Setup rustup..."
 echo "$HOME"
+
 source "$HOME/.cargo/env"
 
 echo "Installing wasm-pack..."
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+
+echo "Run ls -l /vercel/.cargo/bin"
+ls -l /vercel/.cargo/bin
+
+echo "Run ls -l /root/.cargo/bin"
+ls -l /root/.cargo/bin
