9bb9f5704bf77f9d8487b04e0aff22aadcc6cbfd
kanaru-ssk <2019january.0131@gmail.com>
2023-11-21 01:48:54 +0900

    add echo

--- install-wasm-pack.sh
+++ install-wasm-pack.sh
@@ -1,3 +1,8 @@
+echo "Installing rustup..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "/vercel/.cargo/env"
+
+echo "Setup rustup..."
+source "$HOME/.cargo/env"
+
+echo "Installing wasm-pack..."
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
