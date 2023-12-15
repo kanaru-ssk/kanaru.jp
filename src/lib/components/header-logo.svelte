c727a01202f6169b32d2274c0a3900d41c09e03c
kanaru-ssk <2019january.0131@gmail.com>
2023-12-16 01:24:45 +0900

    delete unnecessary space

--- src/lib/components/header-logo.svelte
+++ src/lib/components/header-logo.svelte
@@ -4,6 +4,6 @@
 
 <header class="flex h-16 items-center justify-start px-4 lg:h-24 lg:px-12">
   <a href="/" title="go to home" class="block w-20">
-    <Logo class="h-6 w-fit lg:h-10 " />
+    <Logo class="h-6 w-fit lg:h-10" />
   </a>
 </header>
