262a006c085db9b670943672479ded02515163b9
kanaru-ssk <2019january.0131@gmail.com>
2023-12-16 01:24:25 +0900

    add menu background

--- src/lib/features/menu/sp-menu.svelte
+++ src/lib/features/menu/sp-menu.svelte
@@ -5,6 +5,9 @@
 <label class="relative block h-16 w-16 cursor-pointer">
   <input type="checkbox" name="menu" class="peer hidden" />
   <div
+    class="pointer-events-none fixed left-0 top-0 h-svh w-screen cursor-default bg-black/75 opacity-0 duration-500 peer-checked:pointer-events-auto peer-checked:opacity-100"
+  />
+  <div
     class="absolute left-1/2 top-1/2 h-px w-6 -translate-x-1/2 -translate-y-1 bg-white duration-500 peer-checked:translate-y-0 peer-checked:rotate-45"
   />
   <div
