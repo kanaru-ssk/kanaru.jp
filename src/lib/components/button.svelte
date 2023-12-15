970eba1c1913140d786750d51947f1bc745dd110
kanaru-ssk <2019january.0131@gmail.com>
2023-12-15 12:43:37 +0900

    fix button components

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
   import type { HTMLButtonAttributes } from "svelte/elements";
-  type $$Props = HTMLButtonAttributes;
+
+  let color: "white" | "gray" = "white";
+  let type: HTMLButtonAttributes["type"];
+  export { color, type };
 </script>
 
 <button
-  class={`${$$props.class} h-10 w-36 rounded-full bg-white text-sm text-black hover:opacity-75 active:opacity-50`}
-  on:*
-  {...$$props}
+  {type}
+  class={`${
+    color === "white" ? "bg-white text-black" : "bg-neutral-700 text-white"
+  } h-10 w-36 rounded-full px-8 text-sm hover:opacity-75 active:opacity-50`}
+  on:click
 >
   <slot />
 </button>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import delegateEvents from "svelte-preprocess-delegate-events/preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess(), delegateEvents()],
+  preprocess: [vitePreprocess()],
   kit: { adapter: adapter() },
 };
 
