6056278943386a36714847ac5ba5d39e9de364b1
kanaru-ssk <2019january.0131@gmail.com>
2023-12-18 02:15:59 +0900

    fix class name shrink-0

--- src/lib/features/contact/result-table.svelte
+++ src/lib/features/contact/result-table.svelte
@@ -9,7 +9,7 @@
     <div
       class="flex flex-col space-y-3 border-b border-neutral-400 p-5 first:border-t lg:flex-row lg:items-center lg:space-x-10 lg:space-y-0 lg:p-6"
     >
-      <span class="shink-0 w-28 font-bold lg:text-right">
+      <span class="w-24 shrink-0 font-bold lg:text-right">
         {head}
       </span>
       <span class="w-full">{body}</span>
