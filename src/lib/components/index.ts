f275fdac747c711f2ea406f64b03003dbfd93b44
kanaru-ssk <2019january.0131@gmail.com>
2024-02-21 22:51:38 +0900

    template componentを復活

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -2,4 +2,5 @@ export { default as Button } from "./button.svelte";
 export { default as Logo } from "./logo.svelte";
 export { default as Menu } from "./menu.svelte";
 export { default as Meta } from "./meta.svelte";
+export { default as Template } from "./template.svelte";
 export { default as Title } from "./title.svelte";
