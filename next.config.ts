748a1556ba001677ae2661946c18f99ee9502710
kanaru-ssk <kanaru.ssk@gmail.com>
2025-07-11 00:32:35 +0900

    cp en to root

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	output: "export",
 	images: { unoptimized: true },
-	// i18n: {
-	// 	locales: ["en", "ja"],
-	// 	defaultLocale: "en",
-	// },
 };
 
 export default nextConfig;
