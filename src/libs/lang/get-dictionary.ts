ea68f4630611810b84fd4e80de677fb733256918
kanaru-ssk <kanaru.ssk@gmail.com>
2025-07-11 15:24:11 +0900

    Rendering years dynamically

--- src/libs/lang/get-dictionary.ts
+++ src/libs/lang/get-dictionary.ts
@@ -6,4 +6,22 @@ const dictionaries = {
 	ja: () => import("./ja.json").then((module) => module.default),
 };
 
-export const getDictionary = async (lang: "en" | "ja") => dictionaries[lang]();
+export async function getDictionary(lang: "en" | "ja") {
+	const dictionary = await dictionaries[lang]();
+
+	// Rendering years dynamically
+	dictionary.description = dictionary.description.replace(
+		"$YEARS_OF_EXPERIENCE",
+		getYearsOfExperience().toString(),
+	);
+
+	return dictionary;
+}
+
+function getYearsOfExperience() {
+	const careerStartedAt = new Date("2021-02");
+	const now = new Date();
+	const diffMZ = now.getTime() - careerStartedAt.getTime();
+	const yearsOfExperience = Math.floor(diffMZ / (1000 * 60 * 60 * 24 * 365));
+	return yearsOfExperience;
+}
