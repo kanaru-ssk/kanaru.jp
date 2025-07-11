1f9094d0ef7cf939a67f9a1431cffa488a02e254
kanaru-ssk <kanaru.ssk@gmail.com>
2025-07-11 09:31:12 +0900

    add logo

--- src/components/root-content/root-content.tsx
+++ src/components/root-content/root-content.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import LogoImg from "@/assets/logo.svg";
 import ProfileImg from "@/assets/profile.png";
 import { getDictionary, type Lang } from "@/libs/lang";
 import { LangSwitcher } from "./lang-switcher";
@@ -14,16 +15,19 @@ export async function RootContent({ lang }: RootContentProps) {
 
 	return (
 		<div className="space-y-2 max-w-3xl mx-auto p-5">
-			<LangSwitcher currentLang={lang} />
-			<h1>{dictionary.title}</h1>
+			<div className="flex justify-between">
+				<Image src={LogoImg} alt="logo" priority width={104} height={48} />
+				<LangSwitcher currentLang={lang} />
+			</div>
 			<Image
 				src={ProfileImg}
 				alt="profile"
 				priority
 				width={256}
 				height={256}
-				className="rounded-full my-8"
+				className="rounded-full my-12"
 			/>
+			<h1 className="font-bold text-3xl">{dictionary.title}</h1>
 			<p>{dictionary.description}</p>
 			<SocialLinks />
 			<Skills />
