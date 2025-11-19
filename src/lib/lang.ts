8f1c72f88c2c10a04aa5a51c3c9286774f8e42c7
kanaru-ssk <kanaru.ssk@gmail.com>
2025-11-20 00:29:29 +0900

    add getDictionary func

--- src/lib/lang.ts
+++ src/lib/lang.ts
@@ -5,3 +5,25 @@ export const LANG = {
 export const LANGS = Object.values(LANG);
 export const DEFAULT_LANG = LANG.ENGLISH;
 export type Lang = (typeof LANGS)[number];
+
+export function getDictionary(lang: Lang, yearOfExperience: number) {
+  if (lang === "ja") {
+    return {
+      title: "佐々木 哉瑠",
+      descriptions: [
+        "フリーランスのソフトウェアエンジニアです。",
+        `実務経験 ${yearOfExperience} 年目で、主にGoogleCloud, Go, TypeScript, Next.jsを使用しています。`,
+        "宮城大学の特任助教もしています。",
+      ],
+    };
+  }
+
+  return {
+    title: "Kanaru Sasaki",
+    descriptions: [
+      "I'm a freelance software engineer based in Japan (remote).",
+      `With ${yearOfExperience} years of professional experience, I mainly work with Google Cloud, Go, TypeScript, and Next.js.`,
+      "I'm also an assistant professor at Miyagi University.",
+    ],
+  };
+}
