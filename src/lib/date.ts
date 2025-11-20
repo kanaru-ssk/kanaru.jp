cb99deb2296149e4045cef5d956de958c60e9dbc
kanaru-ssk <kanaru.ssk@gmail.com>
2025-11-20 10:51:11 +0900

    add static analysis

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,4 +1,4 @@
 export function getDiffYear(before: Date, after: Date) {
   const diffMs = after.getTime() - before.getTime();
-  return  Math.floor(diffMs / (1000 * 60 * 60 * 24 * 365));;
+  return Math.floor(diffMs / (1000 * 60 * 60 * 24 * 365));
 }
